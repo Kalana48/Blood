cde9c28044525a310395dd31aa9581bc1c20a631
Kalana48 <149735247+Kalana48@users.noreply.github.com>
2023-12-14 20:53:15 +0000

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,5 +1,5 @@
 <?php 
 $con = mysqli_init();
-mysqli_ssl_set($con,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+mysqli_ssl_set($con,NULL,NULL, "/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 mysqli_real_connect($conn, "bdms.mysql.database.azure.com", "kalana", "Isuru@7794", "bbdms", 3306, MYSQLI_CLIENT_SSL);
 ?>
