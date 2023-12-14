be01ceffbd8e8771d83de173d293cbc161b50f9d
Kalana48 <149735247+Kalana48@users.noreply.github.com>
2023-12-14 21:14:06 +0000

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,4 @@
 <?php 
-$con = mysqli_init();
-mysqli_ssl_set($con,NULL,NULL, "https://github.com/Kalana48/Blood/blob/main/includes/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-mysqli_real_connect($conn, "bdms.mysql.database.azure.com", "kalana", "Isuru@7794", "bbdms", 3306, MYSQLI_CLIENT_SSL);
 
 // Check connection
 if ($conn->connect_error) {
